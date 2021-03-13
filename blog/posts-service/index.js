a5f20bd0260d54ff24287316ae90161dd11ab025
ANISDHIYAEDDINE BOUDIAF <dhiaeboudiaf@gmail.com>
2021-03-13 22:10:29 +0100

    light-weight event-bus added

--- blog/posts-service/index.js
+++ blog/posts-service/index.js
@@ -1,25 +1,46 @@
 const express = require('express');
-const { randomBytes } = require('crypto');
 const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
-posts = {};
+app.use(cors());
+
+const posts = {};
 
 app.get('/posts', (req, res) => {
 	res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
-	let id = randomBytes(4).toString('hex');
-	let { title } = req.body;
+app.post('/posts', async (req, res) => {
+	const id = randomBytes(4).toString('hex');
+	const { title } = req.body;
+
 	posts[id] = {
 		id,
 		title,
 	};
 
+	try {
+		await axios.post('http://localhost:4005/events', {
+			type: 'postCreated',
+			data: {
+				id,
+				title,
+			},
+		});
+	} catch (error) {
+		console.log(error);
+	}
+
 	res.status(201).send(posts[id]);
 });
 
+app.post('/events', (req, res) => {
+	console.log('Event recieved:', req.body.type);
+	res.send({});
+});
 app.listen(4000, () => {
-	console.log('app up and running');
+	console.log('Listening on 4000');
 });
