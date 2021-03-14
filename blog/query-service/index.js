56fb031c61cf57d98efa903176910a81d0151b0d
ANISDHIYAEDDINE BOUDIAF <dhiaeboudiaf@gmail.com>
2021-03-14 13:14:26 +0100

    moderation-service + hotfixes

--- blog/query-service/index.js
+++ blog/query-service/index.js
@@ -1,38 +1,50 @@
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-let posts = {};
-app.get('/posts', (req, res) => {});
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
 
 app.post('/events', (req, res) => {
-	const { type } = req.body;
-	switch (type) {
-		case 'PostCreated': {
-			const { data: { id, title } } = req.body;
-			posts[id] = { id, title, comments: [] };
-			break;
-		}
-		case 'CommentCreated': {
-			const {
-				data: { id, content, postId },
-			} = req.body;
-			posts[postId].comments.push({
-				id,
-				content,
-				postId,
-			});
-			break;
-		}
-	}
+  const { type, data } = req.body;
 
-	console.log(posts);
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
+
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find(comment => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+
+  console.log(posts);
+
+  res.send({});
 });
 
 app.listen(4002, () => {
-	console.log('query service up and running');
+  console.log('Listening on 4002');
 });
